5a85a856aef5a0f5562349cd7dbf559438064630
Davi Federici <dm3788255@gmail.com>
2026-08-29 16:06:10 -0300

    confirmar

--- Estruturas Sequenciais/Lista de Exercicios/ex7.c
+++ Estruturas Sequenciais/Lista de Exercicios/ex7.c
@@ -2,7 +2,37 @@
 
 int main(){
 
-    
+    int hi,mi,hf,mf,c1,c2,c3;
+
+    printf("Digite as horas e os minutos iniciais:\n");
+    scanf("%d%d", &hi , &mi);
+
+    printf("Digite as horas e os minutos finais:\n");
+    scanf("%d%d", &hf , &mf);
+
+    if (hf > hi && mf > mi){
+        c1 = hf - hi;
+        c2 = mf - mi;
+        
+        printf("O JOGO DUROS %d HORA(S) E %d MINUTO(S)\n", c1, c2);
+    }
+    else if (hf == hi && mf == mi){
+        c1 = 24;
+        c2 = 0;
+
+        printf("O JOGO DUROS %d HORA(S) E %d MINUTO(S)\n", c1, c2); 
+    }
+    else if (hf > hi && mi > mf){
+        c1 = hf - hi;
+        c3 = c1 * 60;
+        c2 = c3 + (mf - mi);
+        c1 = 0;
+
+        printf("O JOGO DUROS %d HORA(S) E %d MINUTO(S)\n", c1, c2); 
+    }
+    else{
+        printf("Erro, tente novamente!\n");
+    }
 
     return 0;
 }
